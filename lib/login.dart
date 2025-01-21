be9194b9f8dda7a65e868a0edd17a061a8cafaa5
Vs-coder237 <kengne.bricejunior@ictu.edu.cm>
2025-01-21 14:36:07 +0100

    feat(Login): The designation of the login page

--- lib/login.dart
+++ lib/login.dart
@@ -8,7 +8,32 @@ class Login extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children: [Image.asset('assets/images/nameLogo.png')],
+        mainAxisSize: MainAxisSize.min,
+        children: [
+
+          Image.asset(
+            'assets/images/nameLogo.png',
+            height: 400,
+          ),
+
+          const Text(
+            "Let's Sign In.!",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF202244),
+            ),
+          ),
+
+          const Text(
+            "Login to Your Account to Continue your Courses",
+            style: TextStyle(
+              fontSize: 14,
+              color: Color(0xFF545454),
+            ),
+          ),
+
+        ],
       ),
     );
   }
